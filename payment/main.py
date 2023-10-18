bcc426fa21cf60a31d28d99490d07ccceed1fe59
Dmitry <77066847+Tsuchikage@users.noreply.github.com>
2023-10-18 08:27:39 -0700

    Update main.py

--- payment/main.py
+++ payment/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 import models
-from database import engine
+from database import engine, SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException, Path, Body
-from fastapi.middleware.cors import CORSMiddleware
-from database import SessionLocal
 from starlette import status
 import requests
 
@@ -15,11 +13,7 @@ models.Base.metadata.create_all(bind=engine)
 
 
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    return SessionLocal()
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
@@ -27,7 +21,7 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 @app.get("/get_product_info/{product_id}", status_code=status.HTTP_200_OK)
 async def get_product_info(product_id: int = Path(gt=0)):
-    inventory_service_url = "http://inventory:8000/products/" + str(product_id)
+    inventory_service_url = "http://localhost:8000/products/" + str(product_id)
     response = requests.get(inventory_service_url)
 
     if response.status_code == 200:
@@ -39,7 +33,7 @@ async def get_product_info(product_id: int = Path(gt=0)):
 
 @app.post("/create_order", status_code=status.HTTP_200_OK)
 async def create_order(db: db_dependency, product_id: int = Body(...), product_quantity: float = Body(...)):
-    inventory_service_url = "http://inventory:8000/products/" + str(product_id)
+    inventory_service_url = "http://localhost:8000/products/" + str(product_id)
     response = requests.get(inventory_service_url)
     if response.status_code == 200:
         product_info = response.json()
