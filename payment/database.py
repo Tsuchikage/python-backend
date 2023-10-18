bf9b222dc468b078165c8a2dfeb708d17d414233
Dmitry <77066847+Tsuchikage@users.noreply.github.com>
2023-10-18 08:27:27 -0700

    Update database.py

--- payment/database.py
+++ payment/database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./order.db'
+DATABASE_URL = "postgresql://root:password@localhost:5433/payment"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
